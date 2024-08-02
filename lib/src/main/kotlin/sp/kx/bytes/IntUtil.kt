618d775282d6eb2d737e47de1b2b7150b434e23f
Stanley Wintergreen <kepocnhh@gmail.com>
2024-08-02 15:43:21 +0300

    int util

---
+++ lib/src/main/kotlin/sp/kx/bytes/IntUtil.kt
@@ -0,0 +1,8 @@
+package sp.kx.bytes
+
+fun toInt(b0: Byte, b1: Byte, b2: Byte, b3: Byte): Int {
+    return b0.toInt().and(0xff).shl(24)
+        .or(b1.toInt().and(0xff).shl(16))
+        .or(b2.toInt().and(0xff).shl(8))
+        .or(b3.toInt().and(0xff))
+}
---
+++ lib/src/test/kotlin/sp/kx/bytes/IntUtilTest.kt
@@ -0,0 +1,17 @@
+package sp.kx.bytes
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class IntUtilTest {
+    @Test
+    fun toIntTest() {
+        val expected: Int = 616889394
+        val b0: Byte = 0x24.toByte()
+        val b1: Byte = 0xc4.toByte()
+        val b2: Byte = 0xfc.toByte()
+        val b3: Byte = 0x32.toByte()
+        val actual = toInt(b0 = b0, b1 = b1, b2 = b2, b3 = b3)
+        assertEquals(expected, actual)
+    }
+}
