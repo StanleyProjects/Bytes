72a6568481200347a4d6491678d4bd015f678cd2
Stanley Wintergreen <kepocnhh@gmail.com>
2024-08-03 23:13:39 +0300

    bytes...

---
+++ lib/src/main/kotlin/sp/kx/bytes/Strings.kt
@@ -0,0 +1,17 @@
+package sp.kx.bytes
+
+import java.util.Locale
+
+fun Int.toHEX(locale: Locale = Locale.US): String {
+    return String.format(locale, "%02x", this and 0xff)
+}
+
+fun ByteArray.toHEX(locale: Locale = Locale.US): String {
+    if (isEmpty()) return ""
+    val builder = StringBuilder()
+    builder.append(get(0).toInt().toHEX(locale))
+    for (i in 1 until size) {
+        builder.append(get(i).toInt().toHEX(locale))
+    }
+    return builder.toString()
+}
